3d7b212aaa500065e24d01524d2bb0142c07bc9b
tisljaricleon <lt54120@fer.hr>
2026-01-27 20:51:40 +0100

    cludlet edit

--- files/config.py
+++ files/config.py
@@ -8,8 +8,6 @@ CLOUDLET_EXP = "experiment_2"              # ime eksperimenta za dohvat cloudlet
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
 
-BATCH_SIZE = 64        
-EPOCHS = 20          
 LEARNING_RATE = 0.001 
 
 NUM_NODES = 228       # Broj čvorova u grafu
@@ -18,8 +16,10 @@ NUM_FOR_PREDICT = 12  # Duljina izlaznog vremenskog prozora za predviđanje, 12 
 IN_CHANNELS = 1       # Broj ulaza po čvoru
 NUM_CLIENTS = 2       # Broj klijenata za federativno učenje
 
+BATCH_SIZE = 32        
+EPOCHS = 1          
 NB_BLOCK = 2          # Broj ASTGCN blokova
-K = 3                 # Red Chebyshev polinoma 
-NB_CHEV_FILTER = 64   # Broj filtera u graf konvoluciji 
-NB_TIME_FILTER = 64   # Broj filtera u vremenskoj konvoluciji 
+K = 1                 # Red Chebyshev polinoma 
+NB_CHEV_FILTER = 16   # Broj filtera u graf konvoluciji 
+NB_TIME_FILTER = 16   # Broj filtera u vremenskoj konvoluciji 
 TIME_STRIDES = 1      # Stride u vremenskoj konvoluciji
